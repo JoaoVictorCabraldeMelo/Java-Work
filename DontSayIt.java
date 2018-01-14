6017da27fc5c5014698f154677c0b89300e9bb2e
João Victor Cabral de Melo (NightOff) <joao.victor.cabralmelo@gmail.com>
2018-01-14 17:09:19 -0200

    Não repita comigo horas

---
+++ DontSayIt.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DontSayIt {
+	
+	public static void main (String args[]) {
+		
+		
+		Scanner copy = new Scanner(System.in);
+		
+		System.out.println(copy.nextLine());
+		
+		copy.close();
+		
+	}
+	
+	
+
+}
